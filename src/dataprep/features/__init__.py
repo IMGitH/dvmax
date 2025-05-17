5d6fe0c7563cb7da89c91510907a6804a450f318
IMGitH <mizrahi.itzik@gmail.com>
2025-05-17 06:16:34 +0000

    updated init files

--- src/dataprep/features/__init__.py
+++ src/dataprep/features/__init__.py
@@ -1,3 +1,10 @@
-from src.dataprep.features.price_features import compute_6m_return, compute_volatility
+from src.dataprep.features.price_features import (
+    compute_6m_return, 
+    compute_12m_return, 
+    compute_volatility,
+    compute_max_drawdown,
+    compute_sma_delta
+)
+from src.dataprep.features.metadata_features import encode_sector
 from src.dataprep.features.utils import ensure_date_column, find_nearest_price
 from src.dataprep.feature_pipeline import build_fundamental_features
--- src/dataprep/fetcher/__init__.py
+++ src/dataprep/fetcher/__init__.py
@@ -1,6 +1,7 @@
 from src.dataprep.fetcher.dividends import fetch_dividends
 from src.dataprep.fetcher.prices import fetch_prices
 from src.dataprep.fetcher.ratios import fetch_ratios
+from src.dataprep.fetcher.company import fetch_company_profile
 from src.dataprep.fetcher.fundamentals import (
     fetch_balance_sheet_fund,
     fetch_cashflow_statement_fund,
