8e378ccd51d16eb52574314da2fb52e04084ecba
IMGitH <90335026+IMGitH@users.noreply.github.com>
2025-08-15 18:10:50 +0000

    keep package import side-effect free so macro-only runs don't require FMP_API_KEY

--- src/dataprep/fetcher/__init__.py
+++ src/dataprep/fetcher/__init__.py
@@ -1,10 +1,37 @@
-from src.dataprep.fetcher.ticker_params.dividends import fetch_dividends
-from src.dataprep.fetcher.ticker_params.prices import fetch_prices
-from src.dataprep.fetcher.ticker_params.ratios import fetch_ratios
-from src.dataprep.fetcher.ticker_params.company import fetch_company_profile
-from src.dataprep.fetcher.ticker_params.fundamentals import (
-    fetch_balance_sheet_fund,
-    fetch_cashflow_statement_fund,
-    fetch_income_statement_fund
-    )
-from src.dataprep.fetcher._fmp_client import fmp_get
+def fetch_dividends(*args, **kwargs):
+    from .ticker_params.dividends import fetch_dividends as _impl
+    return _impl(*args, **kwargs)
+
+def fetch_prices(*args, **kwargs):
+    from .ticker_params.prices import fetch_prices as _impl
+    return _impl(*args, **kwargs)
+
+def fetch_ratios(*args, **kwargs):
+    from .ticker_params.ratios import fetch_ratios as _impl
+    return _impl(*args, **kwargs)
+
+def fetch_company_profile(*args, **kwargs):
+    from .ticker_params.company import fetch_company_profile as _impl
+    return _impl(*args, **kwargs)
+
+def fetch_balance_sheet_fund(*args, **kwargs):
+    from .ticker_params.fundamentals import fetch_balance_sheet_fund as _impl
+    return _impl(*args, **kwargs)
+
+def fetch_cashflow_statement_fund(*args, **kwargs):
+    from .ticker_params.fundamentals import fetch_cashflow_statement_fund as _impl
+    return _impl(*args, **kwargs)
+
+def fetch_income_statement_fund(*args, **kwargs):
+    from .ticker_params.fundamentals import fetch_income_statement_fund as _impl
+    return _impl(*args, **kwargs)
+
+def fmp_get(*args, **kwargs):
+    from ._fmp_client import fmp_get as _impl
+    return _impl(*args, **kwargs)
+
+__all__ = [
+    "fetch_dividends", "fetch_prices", "fetch_ratios", "fetch_company_profile",
+    "fetch_balance_sheet_fund", "fetch_cashflow_statement_fund",
+    "fetch_income_statement_fund", "fmp_get",
+]
